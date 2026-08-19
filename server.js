c0aab17d8360389b24bcbf91ebb8d5b4df85f872
youssef sheashia <118205824+youssef-sheashia@users.noreply.github.com>
2026-08-20 00:10:02 +0300

    add socket io

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
-
+import http from "http";
+import { server } from "socket.io";
+import jwt from "jsonwebtoken";
 dotenv.config({ path: "./config.env" });
-
 import Mongoose from "mongoose";
 
 process.on("uncaughtException", (err) => {
@@ -21,7 +22,38 @@ async function startServer() {
   const { default: app } = await import("./app.js");
   ////////////////////////////!!!!!!!!!!!!!!!!!!!!!!!///////////////////////////
   const port = process.env.PORT || 3000;
-  const server = app.listen(port, () => {
+  const httpServer = http.createServer(app);
+  const io = new server(httpServer, {
+    cors: {
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    },
+  });
+  io.use((socket, next) => {
+    const token =
+      socket.handshake.auth?.token ||
+      socket.handshake.headers?.cookie
+        ?.split("; ")
+        .find((c) => c.startsWith("accessToken="))
+        ?.split("=")[1];
+    if (!token) {
+      return next(new Error("Authentication error"));
+    }
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return next(new Error("Authentication error"));
+      }
+      socket.user = decoded;
+      next();
+    });
+  });
+
+  io.on("connection", (socket) => {
+    socket.join(socket.userId);
+    console.log(`user ${socket.userId} connected via socket`);
+  });
+  app.set("io", io);
+  const server = httpServer.listen(port, () => {
     console.log(`server running in port ${port}`);
   });
 
