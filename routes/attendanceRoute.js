555b4b6eb91a39b7e975e02191a3d9d91fc57946
youssef sheashia <118205824+youssef-sheashia@users.noreply.github.com>
2026-08-29 20:44:55 +0300

    add attendance validation

--- routes/attendanceRoute.js
+++ routes/attendanceRoute.js
@@ -7,10 +7,25 @@ import {
 } from "../controllers/attendanceController.js";
 import restrictTo from "../middlewares/restrictTo.js";
 import protect from "../middlewares/protect.js";
+import validate from "../middlewares/validate.js";
+import {
+  attendanceSchema,
+  updateAttendanceSchema,
+} from "../validation/attendanceValidation.js";
 const router = express.Router();
 router.use(protect);
-router.get("/", getAllAttendance);
-router.post("/", restrictTo("security"), markAttendance);
-router.patch("/:id", restrictTo("security"), updateAttendance);
+router.get("/", restrictTo("admin", "hr"), getAllAttendance);
+router.post(
+  "/",
+  restrictTo("security"),
+  validate(attendanceSchema),
+  markAttendance,
+);
+router.patch(
+  "/:id",
+  restrictTo("security"),
+  validate(updateAttendanceSchema),
+  updateAttendance,
+);
 router.get("/my-attendance", getMyAttendance);
 export default router;
