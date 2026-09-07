43dfb96be08c93cb0f626ad9dac43b3218435536
youssef sheashia <118205824+youssef-sheashia@users.noreply.github.com>
2026-09-07 06:05:01 +0300

    add security midelware

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ import employeeRoute from "./routes/employeeRoute.js";
 import departmentRoute from "./routes/departmentRoute.js";
 import taskRoute from "./routes/taskRoute.js";
 import notificationRoute from "./routes/notificationsRoute.js";
+
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import { xss } from "express-xss-sanitizer";
+
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.set("trust proxy", 1);
+app.use(
+  "/api",
+  rateLimit({
+    max: 200,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again later.",
+  }),
+);
+
+app.use(express.json({ limit: "10kb" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/users", userRoute);
